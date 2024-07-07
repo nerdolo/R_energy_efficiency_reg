1533681022048b5b74d2af0a03757679c73f25ef
nerdolo <63171094+nerdolo@users.noreply.github.com>
2024-07-07 15:02:06 +0200

    Update R_regression.R
    Added function for creating scatter plots, added predict() function, improved plot labels

--- R_regression.R
+++ R_regression.R
@@ -16,49 +16,37 @@ data <- mutate_all(data, function(x) as.numeric(as.character(x)))
 # correlation matrix
 data_cor <- data[,c('comp', 'surface', 'wall', 'roof', 'height', 'glazing','heating','cooling')]
 cors <- cor(data_cor)
-corrplot(cors,method = 'number')
+corrplot(cors,method = 'number',main = "Correlation matrix for dataset variables",mar=c(5,4,4,2))
 
 #scatter plots
-x <- data_cor[,'glazing'] 
-y <- data_cor[,'heating']
-model <- lm(y ~ x)
-plot(x, y,
-     xlab="Glazing area", ylab="Heating")
-abline(model, col = 4, lwd = 3)
-lines(lowess(x, y), col = 3, lwd = 3)
+scatter_plot <- function(x,y,xlabel,ylabel, mainlabel){
+  model <- lm(y ~ x)
+  plot(x, y,
+       xlab=xlabel, ylab=ylabel, main = mainlabel)
+  abline(model, col = 4, lwd = 3)
+  lines(lowess(x, y), col = 3, lwd = 3)
+  legend(x="topleft", legend=c("Linear regression line", "Lowess regression curve"),  
+         fill = c(4,3))
+}
 
-x <- data_cor[,'roof'] 
-y <- data_cor[,'heating']
-model <- lm(y ~ x)
-plot(x, y,
-     xlab="Roof area", ylab="Heating")
-abline(model, col = 4, lwd = 3)
-lines(lowess(x, y), col = 3, lwd = 3)
+scatter_plot(data_cor[,'glazing'],data_cor[,'heating'],"Glazing area", "Heating load",
+             "Heating load and glazing area scatter plot")
+scatter_plot(data_cor[,'roof'],data_cor[,'heating'],"Roof area", "Heating load",
+             "Heating load and roof area scatter plot") 
+scatter_plot(data_cor[,'wall'],data_cor[,'heating'],"Wall area", "Heating load",
+             "Heating load and wall area scatter plot")
 
-x <- data_cor[,'wall'] 
-y <- data_cor[,'heating']
-model <- lm(y ~ x)
-plot(x, y,
-     xlab="Wall area", ylab="Heating")
-abline(model, col = 4, lwd = 3)
-lines(lowess(x, y), col = 3, lwd = 3)
 
 #regression model
 model <- lm(heating ~ glazing + roof + wall, data = data)
 summary(model)
 
-#model equation
-# heating = 32.54 + 20.44*glazing - 0.18*roof + 0.05*wall
 
 #estimation table
 estimate_table <- data.frame(glazing=c(0.2,0.3,0.5),
                              roof=c(130,180,200),
                              wall=c(250,300,400))
-estimate = c()
-for(i in 1:nrow(estimate_table)){
-  estimate = append(estimate, 32.54 + 20.44*estimate_table[i,'glazing'] - 0.18*estimate_table[i,'roof'] + 0.05*estimate_table[i,'wall'])
-}
-estimate_table$estimate <- estimate
+estimate_table$estimates <- predict(model, estimate_table)
 
 #added-variable plots
-avPlots(model)
+avPlots(model, id=FALSE, ylab="Heating load | others", main="Added-Variable Plots for glazing area, roof area and wall area")
